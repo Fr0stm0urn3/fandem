c757db4c91895f68f475639143a36c8b8b9f6b63
Cifer0101 <l.kakabadze2020@gmail.com>
2024-09-11 07:27:06 +0400

    Deployment issue #2

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,24 +3,6 @@ import GoBackToBlog from "../../../components/GoBackToBlog"
 import PostContent from "../../../components/PostContent"
 import RelatedArticles from "../../../components/RelatedArticles"
 
-export async function fetchPosts() {
-  try {
-    const res = await fetch(
-      "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@l.kakabadze2020"
-    )
-
-    if (res.status !== 200) {
-      throw new Error("Failed to fetch the data...")
-    }
-
-    const data = await res.json()
-
-    return data
-  } catch (error) {
-    console.log(error)
-  }
-}
-
 const NFTPage = () => {
   return (
     <div className="bg-[#FFF] flex justify-center items-center flex-col">
